51d4e6513bdd866a6f2b665494a403756c9824a8
Kangsik Lee <kangsik.kevin.lee@gmail.com>
2018-02-15 01:27:08 -0800

    Finished Intro

--- src/i_introduction/_0_Hello_World/n00Start.kt
+++ src/i_introduction/_0_Hello_World/n00Start.kt
@@ -20,10 +20,9 @@ fun todoTask0(): Nothing = TODO(
 
         Let's start! Make the function 'task0' return "OK". Note that you can return expression directly.
     """,
-    documentation = doc0(), 
+    documentation = doc0(),
     references = { task0(); "OK" }
+
 )
 
-fun task0(): String {
-    return todoTask0()
-}
+fun task0() = "OK"
